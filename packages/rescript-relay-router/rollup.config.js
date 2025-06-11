9dbc417d263ee6dfeecd2e86f0005008e6e9abef
tsnobip <dontshootthink@gmail.com>
2025-06-11 11:45:10 +0200

    remove dependency to rescript in bundled files

--- packages/rescript-relay-router/rollup.config.js
+++ packages/rescript-relay-router/rollup.config.js
@@ -18,7 +18,7 @@ export default [
       format: "esm",
       name: "RescriptRelayVitePlugin",
     },
-    plugins: [resolve({ resolveOnly: [/^@rescript\/.*$/] })],
+    plugins: [resolve({ resolveOnly: ["@rescript/core", "rescript"] })],
     external: ["fsevents"],
   },
   {
@@ -28,7 +28,7 @@ export default [
       format: "esm",
       name: "RescriptRelayServerVitePlugin",
     },
-    plugins: [resolve({ resolveOnly: [/^@rescript\/.*$/] })],
+    plugins: [resolve({ resolveOnly: ["@rescript/core", "rescript"] })],
     external: ["fsevents", "vite"],
   },
 ];
